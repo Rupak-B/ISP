f5d4d1a75898905f5620afa92bbb11801709a25a
Rupak Bhattacharya <rupak@bhattacharya.org>
2022-04-29 08:05:01 -0500

    dsf

--- ISPwork/Sources/ScenesShell/InteractionLayer.swift
+++ ISPwork/Sources/ScenesShell/InteractionLayer.swift
@@ -10,7 +10,8 @@ import Igis
 class InteractionLayer : Layer {
 
     let projectileExample : Projectile
-
+    let ufo = UFO()
+    let ufo2 = UFO()
     init() {
         self.projectileExample = Projectile(velocityY : 3, rectangle: Rectangle(rect:Rect(topLeft: Point(), size:Size(width: 4, height: 30)), fillMode: .fill))
         
@@ -18,7 +19,11 @@ class InteractionLayer : Layer {
         super.init(name:"Interaction")
         
         // We insert our RenderableEntities in the constructor
+        insert(entity: ufo, at: .front)
+        ufo.changeVelocity(velocityX: 26, velocityY: 12)
         insert(entity: projectileExample, at: .front)
+        insert(entity: ufo2, at: .front)
+        ufo2.changeVelocity(velocityX: -20, velocityY: 12)
     }
 
     func removeEntity(entity: RenderableEntity) {
